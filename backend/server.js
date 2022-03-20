396bb9a99468cebc1306a974ddea4861ae255e02
VladimirVolvakov <dr.volvakov@gmail.com>
2022-03-20 23:01:24 +0300

    feat: add error handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+const { errorHandler } = require('./middleware/errorMiddleware')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
 
 const app = express()
+
+// Body parser middleware:
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -12,5 +17,7 @@ app.get('/', (req, res) => {
 
 // Routes:
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
