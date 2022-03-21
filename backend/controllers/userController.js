58ad3772b0371c1973155ff2c47243de5f70656a
VladimirVolvakov <dr.volvakov@gmail.com>
2022-03-21 16:57:38 +0300

    feat: add user log in and token generation functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
 // @description:    Register a new user
@@ -38,7 +39,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user._id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -50,8 +52,32 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route:          /api/users/login
 // @access:         public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route')
+    // Get user data from request body:
+    const { email, password } = req.body
+
+    // Check if user with entered email is already registered:
+    const userExists = await User.findOne({email})
+
+    // Check if user's password is matching to that in database:
+    if (userExists && (await bcryptcompare(password, userExists.password))) {
+        res.status(200).json({
+            _id: userExists._id,
+            name: userExists.name,
+            email: userExists.email,
+            token: generateToken(userExists._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error('Please enter correct email and password')
+    }
 })
+
+// Generate token:
+const generateToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
 
 module.exports = {
     registerUser,
