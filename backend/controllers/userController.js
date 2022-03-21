cdec77a270438223c0ad3db21cb1161ca489422a
VladimirVolvakov <dr.volvakov@gmail.com>
2022-03-21 16:01:44 +0300

    feat: add register user in db functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
 
 // @description:    Register a new user
 // @route:          /api/users
@@ -12,7 +14,36 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please fill in all required fields')
     }
-    res.send('Register Route')
+
+    // Find if user is already registered:
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User with entered email already exists')
+    }
+
+    // Hash password:
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create a new user:
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Something went wrong...')
+    }
 })
 
 // @description:    Login a user
